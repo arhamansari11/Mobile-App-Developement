028939dfe82da82a83f302fead25eb2d67659867
Arham <arhamansari454647@gmail.com>
2023-11-27 17:48:31 +0500

    Class-03 Folder Structure and Styling in Native

---
+++ Class 03/src/Components/headerStyle.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+const viewvar = StyleSheet.create({
+    viewvarq: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+  
+  const styles1 = StyleSheet.create({
+    container: {
+      color: "white",
+      fontWeight: "bold",
+    },
+  });
+  const styles2 = StyleSheet.create({
+    container: {
+      color: "white",
+      fontWeight: "bold",
+    },
+  });
+  
