d021e41a1f9ed6761e66ec8736639c1bbce096c5
Arham <arhamansari454647@gmail.com>
2023-12-06 10:43:13 +0500

    Class-04 ( Navigations in Native {Stack,Tab,Drawyer})

--- Class 04/navigation-app/components/Header.jsx
+++ Class 04/navigation-app/components/Header.jsx
@@ -7,7 +7,7 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 const Header = () => {
   return (
     <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Open up App.js to start working on your app</Text>
     <StatusBar style="auto" />
     <TouchableOpacity
       onPress={() => alert('This is a button!')}
